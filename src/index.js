592644a5414a2ede5e5b2831db9fa0c34677baab
Ivo-Jr <maciel.jr95@gmail.com>
2021-03-02 13:55:03 -0300

    SPA finished

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Cadastro';
 
 
 
@@ -15,6 +16,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component={Home} exact/>
       <Route path="/cadastro/video" component={CadastroVideo} />  
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />  
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
--- src/pages/cadastro/Cadastro/index.js
+++ src/pages/cadastro/Cadastro/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
-export default function CadastroVideo() {
+export default function CadastroCategoria() {
     return (
     <PageDefault>
-      <h1>Cadastro de Video</h1>
+      <h1>Cadastro de Categoria</h1>
 
-      <Link to="/cadastro/categoria">
-        Cadastrar Categoria
+      <Link to="/">
+        Ir para home
       </Link>
     </PageDefault>
     )
